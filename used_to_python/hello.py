d12794b2bc009213051414f370f4eb43a3eba33d
Epic <fire.star.2000@hotmail.com>
2021-07-19 16:09:04 -0400

    yes

---
+++ used_to_python/hello.py
@@ -0,0 +1,21 @@
+#! /usr/bin/env  python3
+from math import *
+name = "potato"
+print("hello")
+print("potato")
+print(name.index("o",2))
+print(name.replace("o","a"))
+number = 55
+numbers = [50,5,3,5]
+number2 = str(55)
+
+print(number2 + " yes")
+
+print(abs(-5))
+print(min(numbers))
+print(max(1,2,5))
+print(round(3.62))
+print(bin(12))
+names = input("tell me your name you fat fuck : ")
+print(names + " nice name faggot")
+
